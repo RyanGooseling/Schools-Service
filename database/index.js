938c03ec3022fd81f9584294cc049579ad55013c
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-25 16:30:26 -0600

    Makes more changes to the db setup

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoUri = 'mongodb://localhost/bluefin-schools';
+
+const db = mongoose.connect(mongoUri);
+
+module.exports = db;
+
+
+// more code here
+
+// changes on db-setup
+
+// more changes on db-setup
+
+// more more changes on db-setup
