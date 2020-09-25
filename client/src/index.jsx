2e2694731b63ad862a511c92fa4b5d9d312367cb
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-24 20:40:46 -0600

    Sets up npm packages, webpack, airbnb linter, and basic React render

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <h2>Rendered Index</h2>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ readme.md
@@ -0,0 +1,5 @@
+# BlueFin
+A redfin clone
+
+## School Service
+This repo will create the functionality of the schools widget on the Redfin site, plus providing the functionality to create new school reviews.
