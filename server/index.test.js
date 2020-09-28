9f296a0b7eaf2cfbd7a705e54bac3ddb8b53f705
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-28 14:56:20 -0700

    Refactors jest test to use promises

--- server/index.test.js
+++ server/index.test.js
@@ -1,6 +1,6 @@
 /**
  * @jest-environment node
- */
+*/
 
 const axios = require('axios');
 
@@ -16,8 +16,14 @@ afterAll(() => {
   server.close();
 });
 
-test('/schools endpoint should have status 200 and response of 5', async () => {
-  const { data, status } = await api.get('/homes/5/schools');
-  expect(status).toBe(200);
-  expect(data).toEqual('5');
+test('/schools endpoint should have status 200 and response of 5', (done) => {
+  api.get('/homes/5/schools')
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.data).toEqual('5');
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
 });
