5e7626f21a056ae128702571a6b4403cdabead1b
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-29 16:07:53 -0700

    Renders proper fields

--- client/src/components/SchoolItem.jsx
+++ client/src/components/SchoolItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const SchoolItem = (props) => {
-  console.log(props);
+  const computeRating = (ratingObj) => {
+    return Math.round((ratingObj.lowIncome + ratingObj.studentGrowth + ratingObj.testScores + ratingObj.equity) / 4);
+  };
+
+  const computeReviewScore = (reviewsArr) => {
+    const reviewScoreSum = reviewsArr.reduce((acc, current) => acc + current.score, 0);
+    return Math.round(reviewScoreSum / reviewsArr.length);
+  };
+
   return (
     <div>
-      { props.school.name }
+      <p>{ computeRating(props.school.rating) } / 10</p>
+      <p>{ props.school.name }</p>
+      <p>{ props.school.type } • { props.school.grades } • { props.school.choiceSchool }</p>
+      <p>{ computeReviewScore(props.school.reviews) } stars with { props.school.reviews.length } reviews</p>
+      <p>{props.school.distance} mi Distance</p>
     </div>
   );
 };
