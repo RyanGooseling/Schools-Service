d39cf9066bfe97dd57252a89297f9548da5c613b
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-29 13:12:16 -0700

    Updates index.js tests to account for db query

--- server/controllers/schoolsController.js
+++ server/controllers/schoolsController.js
@@ -3,21 +3,14 @@ const schoolModel = require('../../database/School.js');
 const schoolsController = {};
 
 schoolsController.get = (req, res) => {
-  console.log('in the controller');
-
   schoolModel.find({ property_id: Number(req.params.id) }).sort({ distance: 1 }).limit(3)
     .then((schools) => {
-      console.log('in the then');
-      // console.log(schools);
       res.json(schools);
     })
     .catch((err) => {
       console.log('error');
-      console.log(err);
+      res.json(err);
     });
-
-  // res.json(req.params.id);
-  // res.end();
 };
 
 module.exports = schoolsController;
--- server/index.test.js
+++ server/index.test.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 const server = require('./index');
 
-const api = axios.create({ baseURL: 'http://localhost:3002' });
+const api = axios.create({ baseURL: 'http://localhost:9002' });
 
 beforeAll(() => {
   server.start(9002);
@@ -16,14 +16,47 @@ afterAll(() => {
   server.close();
 });
 
-test('/schools endpoint should have status 200 and response of 5', (done) => {
+test('/schools endpoint should have status 200 and response of 3 items', (done) => {
   api.get('/homes/5/schools')
     .then((res) => {
       expect(res.status).toBe(200);
-      expect(res.data).toEqual('5');
+      expect(res.data.length).toEqual(3);
       done();
     })
     .catch((err) => {
       done(err);
     });
 });
+
+test('/schools endpoint should return an array', (done) => {
+  api.get('/homes/5/schools')
+    .then((res) => {
+      expect(Array.isArray(res.data)).toBe(true);
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+test('/schools endpoint should return an array of items in order of shortest distance', (done) => {
+  api.get('/homes/5/schools')
+    .then((res) => {
+      expect(res.data[0].distance).toBeLessThanOrEqual(res.data[1].distance);
+      expect(res.data[1].distance).toBeLessThanOrEqual(res.data[2].distance);
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+// test('/schools endpoint should return an array', (done) => {
+//   return api.get('/homes/5/schools')
+//     .then((res) => {
+//       console.log(res.data);
+//       console.log(Array.isArray(res.data));
+//       expect(Array.isArray(res.data)).toBe(true);
+//       done();
+//     });
+// });
