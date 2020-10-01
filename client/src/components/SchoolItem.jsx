8d623cf3cc5f251110b3d7ab25e500f3d7e27968
Muhammad Meigooni <mmeigooni@gmail.com>
2020-09-30 21:08:10 -0700

    Updates styles for Schools

--- client/src/components/SchoolItem.jsx
+++ client/src/components/SchoolItem.jsx
@@ -5,19 +5,63 @@ const SchoolItem = (props) => {
     return Math.round((ratingObj.lowIncome + ratingObj.studentGrowth + ratingObj.testScores + ratingObj.equity) / 4);
   };
 
-  const computeReviewScore = (reviewsArr) => {
+  const starPath = (reviewsArr) => {
+    const map = {
+      0: 'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+    };
     const reviewScoreSum = reviewsArr.reduce((acc, current) => acc + current.score, 0);
-    return Math.round(reviewScoreSum / reviewsArr.length);
+    const averageStarsNum = Math.round(reviewScoreSum / reviewsArr.length);
+    return `./assets/${map[averageStarsNum]}Star.png`;
   };
 
   return (
-    <div>
-      <p>{ computeRating(props.school.rating) } / 10</p>
-      <p>{ props.school.name }</p>
-      <p>{ props.school.type } • { props.school.grades } • { props.school.choiceSchool }</p>
-      <p>{ computeReviewScore(props.school.reviews) } stars with { props.school.reviews.length } reviews</p>
-      <p>{props.school.distance} mi Distance</p>
-    </div>
+    <tr className="schools-table-row">
+      <td className="rating-and-name-col">
+        <div className="rating-col">
+          <div className="ellipse">
+            <div className="rating-text">
+              <span className="rating-num">{ computeRating(props.school.rating) }</span>
+              <span className="out-of"> / 10</span>
+            </div>
+          </div>
+        </div>
+        <div className="name-col">
+          <div className="name-and-info">
+            <div className="school-name font-size-base">
+              <div className="school-title">
+                { props.school.name }
+              </div>
+              <div className="sub-info font-size-smaller">
+                { props.school.type } • { props.school.grades } • { props.school.choiceSchool }
+              </div>
+            </div>
+          </div>
+        </div>
+      </td>
+      <td className="num-students-col">
+        <div className="students">
+          { props.school.studentCount }
+        </div>
+        <div className="sub-info font-size-small">
+          Students
+        </div>
+      </td>
+      <td className="rating-col">
+        <div className="star-rating">
+          <img src={starPath(props.school.reviews)} alt="" />
+          <div className="sub-info font-size-smaller num-reviews">{ props.school.reviews.length } reviews</div>
+        </div>
+      </td>
+      <td className="distance-col">
+        <div className="value">{props.school.distance} mi</div>
+        <div className="sub-info font-size-small">Distance</div>
+      </td>
+    </tr>
   );
 };
 
--- client/src/components/Schools.jsx
+++ client/src/components/Schools.jsx
@@ -4,7 +4,8 @@ import SchoolItem from './SchoolItem.jsx';
 const Schools = (props) => {
   // console.log(props.schools[0]._id);
   return (
-    <div>
+    <div className="schools-table">
+
       {props.schools.map((school) => <SchoolItem school={school} key={school._id} />)}
     </div>
   );
