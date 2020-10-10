c6203c9d3954d5bfaaef814e6b9741077cb58b40
Muhammad Meigooni <mmeigooni@gmail.com>
2020-10-09 19:57:00 -0600

    Serves assets at /assets

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Schools from './components/Schools.jsx';
 
+const Font = styled.div`
+  font-family: libreFranklin;
+  color: #333;
+`;
+
+const SchoolsHeader = styled.h2`
+  padding-left: 0;
+  width: 100%;
+  word-spacing: .125rem;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
 const SchoolService = () => (
-  <Router>
-    <h2>Schools</h2>
-    <Route path="/homes/:id" component={Schools} />
-  </Router>
+  <Font>
+    <Router>
+      <SchoolsHeader>Schools</SchoolsHeader>
+      <Route path="/homes/:id" component={Schools} />
+    </Router>
+  </Font>
 );
 
 // window.SchoolsService = SchoolService;
