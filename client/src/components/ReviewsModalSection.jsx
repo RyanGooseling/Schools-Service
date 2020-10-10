ecb3d950fa09015d8d9f20d5d62cf63c157c1748
Muhammad Meigooni <mmeigooni@gmail.com>
2020-10-09 20:18:56 -0600

    Adds styled components to GreatSchoolsModalSection, RatioModalSection, and ReviewsModalSection

--- client/src/components/ReviewsModalSection.jsx
+++ client/src/components/ReviewsModalSection.jsx
@@ -3,7 +3,13 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import styled from 'styled-components';
 import ReviewItem from './ReviewItem.jsx';
+
+const NewReview = styled.p`
+  color: #1080a2;
+  cursor: pointer;
+`;
 
 const ReviewsModalSection = ({school, changeView}) => {
   const {reviews} = school;
@@ -15,13 +21,12 @@ const ReviewsModalSection = ({school, changeView}) => {
     } else {
       return 0;
     }
-    // return (a.createdAt < b.createdAt) ? 1 : ((a.createdAt > b.createdAt) ? -1 : 0);
   });
   return (
     <div className="reviews">
       <h4>Community Reviews ({reviews.length})</h4>
       {reviews.map((review, index) => <ReviewItem review={review} key={index} />)}
-      <p className="new-review" onClick={() => {changeView('new-review')}}>Add new review</p>
+      <NewReview onClick={() => {changeView('new-review')}}>Add new review</NewReview>
     </div>
   );
 };
